bddbbf5768065441aa477375f98aee0188939819
dongyoon7212 <dongyoon7212@naver.com>
2025-08-08 21:02:02 +0900

    Feat: react

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import BasicJsx from "./components/BasicJsx/BasicJsx";
 import BasicProps from "./components/BasicProps/BasicProps";
 import BasicReact from "./components/BasicReact/BasicReact";
+import Calculator from "./components/BasicState/Calculator/Calculator";
 import CountState from "./components/BasicState/CountState/CountState";
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
 			{/* <BasicReact /> */}
 			{/* <BasicJsx /> */}
 			{/* <BasicProps /> */}
-			<CountState />
+			{/* <CountState /> */}
+			<Calculator />
 		</>
 	);
 }
