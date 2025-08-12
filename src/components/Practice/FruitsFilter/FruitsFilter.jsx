a1cadcadee4b8e7acb0994e84950a962b7feae9f
dongyoon7212 <dongyoon7212@naver.com>
2025-08-12 21:47:34 +0900

    Feat: react practice

---
+++ src/components/Practice/FruitsFilter/FruitsFilter.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+const fruits = [
+	"Apple",
+	"Banana",
+	"Cherry",
+	"Grape",
+	"Orange",
+	"Strawberry",
+	"Watermelon",
+];
+
+function FruitsFilter() {
+	const [filterFruits, setFilterFruits] = useState(fruits);
+	const [inputValue, setInputValue] = useState("");
+
+	useEffect(() => {
+		const newFilterFruits = fruits.filter((fruit) =>
+			fruit.toLowerCase().includes(inputValue.toLowerCase())
+		);
+		console.log("새로운 필터된 배열:", newFilterFruits);
+		setFilterFruits(newFilterFruits);
+
+		console.log("마운트 됨");
+
+		return () => {
+			console.log("언마운트 됨");
+		};
+	}, [inputValue]);
+
+	return (
+		<div>
+			<input
+				type="text"
+				onChange={(e) => setInputValue(e.target.value)}
+			/>
+			<ul>
+				{filterFruits.map((fruit, index) => (
+					<li key={index}>{fruit}</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default FruitsFilter;
+
+//input하나 만들고 ul하나 만들고 input에 과일이름을 검색해서 필터를 거친뒤에 li로 출력
+//최초에는 전체 과일을 보여주고 내가 입력을 할때마다 즉시 filter를 거쳐서 li로 보여주기
+//대소문자 상관없이
